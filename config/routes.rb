9968cb51137574c25a709d261bc94a49c9a0b0b2
fher03 <feransic@gmail.com>
2025-03-19 18:14:27 -0600

    15 Internacionalizacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "products#index"
-  get "/products", to: "products#index"
-  get "/products/new", to: "products#new"
-  get "/products/:id", to: "products#show"
-  post "/products", to: "products#create"
+
+  resources :products
 end
